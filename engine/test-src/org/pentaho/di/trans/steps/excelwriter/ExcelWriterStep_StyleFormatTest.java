349ddef77bd752619a002817d6ca4ff1ae017a00
Amin Khan <amin.khan@git.rocks>
2016-06-27 13:19:46 +0100

    Code copied from ExcelOutput and ExcelWriter tests

--- engine/test-src/org/pentaho/di/trans/steps/excelwriter/ExcelWriterStep_StyleFormatTest.java
+++ engine/test-src/org/pentaho/di/trans/steps/excelwriter/ExcelWriterStep_StyleFormatTest.java
@@ -22,6 +22,23 @@
 
 package org.pentaho.di.trans.steps.excelwriter;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.logging.LoggingObjectInterface;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.steps.StepMockUtil;
+import org.pentaho.di.trans.steps.mock.StepMockHelper;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 /**
  * @author Amin Khan
  */
@@ -29,6 +46,48 @@ public class ExcelWriterStep_StyleFormatTest {
 
   private ExcelWriterStep step;
   private ExcelWriterStepData data;
+  private static StepMockHelper<ExcelWriterStepMeta, ExcelWriterStepData> helper;
 
+  @BeforeClass
+  public static void setUpEnv() throws KettleException {
+    KettleEnvironment.init();
+    helper =
+        new StepMockHelper<ExcelWriterStepMeta, ExcelWriterStepData>( "ExcelWriterStep_StyleFormatTest", ExcelWriterStepMeta.class,
+                ExcelWriterStepData.class );
+    when( helper.logChannelInterfaceFactory.create( any(), any( LoggingObjectInterface.class ) ) ).thenReturn(
+            helper.logChannelInterface );
+    when( helper.trans.isRunning() ).thenReturn( true );
+  }
 
+  @Before
+  public void setUp() throws Exception {
+    StepMockHelper<ExcelWriterStepMeta, StepDataInterface> mockHelper =
+            StepMockUtil.getStepMockHelper( ExcelWriterStepMeta.class, "ExcelWriterStep_StyleFormatTest" );
+
+    step = new ExcelWriterStep(
+            mockHelper.stepMeta, mockHelper.stepDataInterface, 0, mockHelper.transMeta, mockHelper.trans );
+    step = spy( step );
+    // ignoring to avoid useless errors in log
+    doNothing().when( step ).prepareNextOutputFile();
+
+    data = new ExcelWriterStepData();
+
+    step.init( mockHelper.initStepMetaInterface, data );
+  }
+
+  @Test
+  public void generate_Hssf() throws Exception {
+    data.wb = new HSSFWorkbook();
+    data.wb.createSheet( "sheet1" );
+    data.wb.createSheet( "sheet2" );
+
+  }
+
+  @Test
+  public void generate_Xssf() throws Exception {
+    data.wb = new XSSFWorkbook();
+    data.wb.createSheet( "sheet1" );
+    data.wb.createSheet( "sheet2" );
+
+  }
 }
